f5b712957ac83bf031e816dba6b23e23c3aabe7e
Hadid20 <gustihadid56@gmail.com>
2025-10-03 11:17:43 +0700

    Update 1

---
+++ script.js
@@ -0,0 +1,27 @@
+function Hitung() {
+  let num1 = Number(document.getElementById("num1").value);
+  let num2 = Number(document.getElementById("num2").value);
+  let operator = Number(document.getElementById("operator").value);
+
+  let hasil;
+
+  switch (operator) {
+    case 1:
+      hasil = num1 + num2;
+      break;
+    case 2:
+      hasil = num1 - num2;
+      break;
+    case 3:
+      hasil = num1 * num2;
+      break;
+    case 4:
+      hasil = num1 / num2;
+      break;
+    default:
+      hasil = "input tidak valid";
+      break;
+  }
+
+  document.getElementById("hasilp").innerHTML = hasil;
+}
